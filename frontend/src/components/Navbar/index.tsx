2aa8348b7d3fa45e25eedf12c46e2cb7ffd4fc15
Pedro Henrique <pedrohenrique12101210@gmail.com>
2024-11-27 19:41:15 -0300

    Navbar Criada

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,15 +1,17 @@
 import { ReactComponent as IconeGitHub} from '../../assets/img/gitHub.svg';
 
+import './style.css';
+
 function Navbar(){
     return(
         <header>
         <nav className="container">
-          <div>
+          <div className= "movies-nav-content">
             <h1>Catálogo Movies</h1>
             <a href="https://github.com/Pedron1210">
-            <div>
+            <div className='movies-repository-container'>
               <IconeGitHub/>
-              <p>/GitHub</p>
+              <p className='movies-repository-link'>/GitHub</p>
               
             </div>
             </a>
