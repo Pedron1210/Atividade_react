fc5ef899b81e212119f16d27d616e1ff0559fee8
Pedro Henrique <pedrohenrique12101210@gmail.com>
2024-11-28 18:32:18 -0300

    Navbar Criada

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,46 @@
+import './style.css';
+
+function Form() {
+  const movies = {
+    id: 1,
+    image:
+    "https://th.bing.com/th/id/OIP.AHpzYF6H-XiYy5sVr2225QHaKo?w=189&h=272&c=7&r=0&o=5&pid=1.7",
+    title: "O gladiador",
+    count: 2,
+    score: 4.0,
+  };
+  return (
+    <div className="movies-form-container">
+      <img src={movies.image} alt={movies.title} />
+      <div className="movies-card-bottom-container">
+        <h3>{movies.title}</h3>
+        <form className="movies-form">
+          <div className="form-group movies-form-group">
+            <label htmlFor="email">Informe seu email</label>
+            <input className="form-control" type="email" id="email" />
+          </div>
+          <div className="form-group movies-form-group">
+            <label htmlFor="score">Informe sua avaliação</label>
+            <select className="form-control" id="score">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div className="movies-form-btn-container">
+            <button className="mosvies-btn" type="submit">
+              Salvar
+            </button>
+          </div>
+        </form>
+        <a className="mosvies-btn" href="index.html">
+          Cancelar
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing(){
+    return(
+        <h1>Página Lista</h1>
+    )
+}
+
+export default Listing;
